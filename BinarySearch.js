99f5dbceab35ad5a7183189c3d8573e3fbba33f1
mahimaagrawal2510 <62229482+mahimaagrawal2510@users.noreply.github.com>
2025-08-11 09:19:02 +0000

    Binary search

--- BinarySearch.js
+++ BinarySearch.js
@@ -1,5 +1,20 @@
 //array should be sorted
-function binarySearch(num=0, arr=[]){
 
+function binarySearch(num=0, arr=[]){
+    let left = 0;
+    let right = arr.length-1;
+   
+    while(left<=right){
+        let mid = Math.floor((left + right)/2)
+        if(arr[mid] == num){
+            return "Element found at " + mid + " index"
+        }
+        else if(arr[mid] > num){
+            right = mid-1
+        }else if(arr[mid] < num){
+            left = mid+1
+        }
+    }
+    return "Element not found";
 }
-binarySearch()
+binarySearch(59, [5,9,17,23,25,45,59,63,71,89])
