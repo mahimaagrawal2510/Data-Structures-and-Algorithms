c8698a2ce3bb27d50bd6b223504c04205bfa38ce
mahimaagrawal2510 <62229482+mahimaagrawal2510@users.noreply.github.com>
2025-08-21 18:38:51 +0000

    valid anagram

---
+++ Leetcode/ValidAnagram.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length != t.length) return false;
+    let hash = {};
+    for(let i=0;i<s.length;i++){
+        if(hash[s[i]]){
+            ++hash[s[i]];
+        }else{
+            hash[s[i]] = 1;
+        }
+    }
+    for(let i=0;i<t.length;i++){
+        if(hash[t[i]]){
+            hash[t[i]]--; 
+        }else{
+            return false
+        }
+    }
+    return true;
+  
+};
+
+
